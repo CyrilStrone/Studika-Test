a94dee4a3034878cffbd6d31199b06a4d8000fe3
Cyril_Strone <kidvos@gmail.com>
2022-04-09 11:41:16 +0700

    update

--- js/morris.js
+++ js/morris.js
@@ -4,7 +4,7 @@ new Morris.Area({
     // Chart data records -- each entry in this array corresponds to a point on
     // the chart.
     data: [
-        { year: '2012', budget: 110, pay: 0 },
+        { year: '2012', budget: 110, pay: null },
         { year: '2013', budget: 200, pay: 100 },
         { year: '2014', budget: 105, pay: 140 },
         { year: '2015', budget: 220, pay: 220 },
@@ -12,12 +12,19 @@ new Morris.Area({
         { year: '2017', budget: 140, pay: 170 },
         { year: '2018', budget: 205, pay: 20 },
         { year: '2019', budget: 185, pay: 20 },
-        { year: '2020', budget: 0, pay: 0 },
+        { year: '2020', budget: null, pay: null },
         { year: '2021', budget: 200, pay: 150 },
     ],
     xkey: 'year',
-    ykeys: ['budget', 'pay'],
+    ykeys: ['pay', 'budget'],
     labels: ['Проходной балл на бюджет', 'Проходной балл на платное'],
+    lineColors: ['#cddef2', '#6a9ad2'],
+    lineWidth: 2,
     pointSize: 2,
-    hideHover: 'auto'
+    hideHover: 'always',
+    smooth: false,
+    fillOpacity: 0.5,
+    parseTime: false,
+    xLabels: "year"
+
 });
